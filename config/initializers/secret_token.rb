f393bba4d300d2dfc70587319dc6226af3d69722
jeffpersonified <jeff.matthew.smith@gmail.com>
2013-06-29 15:08:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrafficWatch::Application.config.secret_token = '8d9baab536a42ebc64e8834fe9b62d6bb9d4d01282856cbf7c6960fba88a8c5cb939eaaa009dfd619fd990c4b4639d681dd084562a1dc7e3f65c9cd44d101f2d'
