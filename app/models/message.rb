4f9fef76d5e8642aa791eae1e732cf05848e390a
jeffpersonified <jeff.matthew.smith@gmail.com>
2013-06-29 18:14:08 -0700

    really add message details

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,21 +1,24 @@
 class Message < ActiveRecord::Base
-  attr_accessible :campaign_id, :content
+  attr_accessible :campaign_id, :content, :from, :to, :city, :state, :zip, :country,
+                  :date_sent, :price, :uri
+
   belongs_to :campaign
-  # validates_presence_of :campaign_id, :content
+  # validates_presence_of :campaign_id
 
   def self.process_sms(params)
-    puts "CALL RECEIVED ------> Params: #{params}"
+    puts "CALL RECEIVED @ Params: #{params}"
 
-# CALL RECEIVED ------> Params: {"AccountSid"=>"AC681c370b6a0881b2a46c946d3ba38e94", "Body"=>"Heyo!", "ToZip"=>"", "FromState"=>"MI", "ToCity"=>"", "SmsSid"=>"SM085adde77f31110f2ddac56d2c26359d", "ToState"=>"CA", "To"=>"+14155285688", "ToCountry"=>"US", "FromCountry"=>"US", "SmsMessageSid"=>"SM085adde77f31110f2ddac56d2c26359d", "ApiVersion"=>"2010-04-01", "FromCity"=>"GRAND RAPIDS", "SmsStatus"=>"received", "From"=>"+16166482327", "FromZip"=>"49504", "controller"=>"twilio", "action"=>"process_sms"}
     from          = params[:From]
     to            = params[:To]
-    content          = params[:Body]
+    content       = params[:Body]
     # date_sent     = params[:From]
-    # price          = params[:From]
-    # uri          = params[:From]
+    # price         = params[:From]
+    # uri           = params[:From]
     city          = params[:FromCity]
-    state          = params[:FromState]
-    zip          = params[:FromZip]
-    Message.create(content: content, city: city, state: state, zip: zip, to: to, from: from)
+    state         = params[:FromState]
+    zip           = params[:FromZip]
+    country       = params[:FromCountry]
+
+    Message.create(city: city, state: state, zip: zip, to: to, from: from, content: content, country: country)
   end
 end
