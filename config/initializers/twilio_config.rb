639990aa2a67116fc0f2f9ef148d9b845fd1baff
jeffpersonified <jeff.matthew.smith@gmail.com>
2013-06-29 20:22:14 -0700

    test without config phone number

--- config/initializers/twilio_config.rb
+++ config/initializers/twilio_config.rb
@@ -1,3 +1,3 @@
 TrafficWatch::Application.config.account_sid = "ACcc93da6d060bdfd9051c767e683be9dc"
 TrafficWatch::Application.config.auth_token = "eadf3061d61133049239883c3fdce33f"
-TrafficWatch::Application.config.phone_number = "(415)528-5688"
+# TrafficWatch::Application.config.phone_number = "(415)528-5688"
